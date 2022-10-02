56c222129e5437aa515ab9de701911cfdee1d06f
EgorKoroteev <gof.piter@mail.ru>
2022-10-02 21:37:52 +0500

    Before Test data edit

--- SalaryFond/SalaryFond/ViewModels/MainWindowViewModel.cs
+++ SalaryFond/SalaryFond/ViewModels/MainWindowViewModel.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SalaryFond.Services;
+using SalaryFond.Models;
 
 namespace SalaryFond.ViewModels
 {
     internal class MainWindowViewModel : ViewModelBase
     {
         public AddCompanyViewModel AddCompany { get; }
+
+        private WorkersManager _WorkersManager;
 
         private string _title = "Главное окно";
 
@@ -19,10 +23,12 @@ namespace SalaryFond.ViewModels
             set => Set(ref _title, value);
         }
 
-        public MainWindowViewModel(AddCompanyViewModel NewCompany)
+        public IEnumerable<Company> Companies => _WorkersManager.Companies;
+        public IEnumerable<Worker> Workers => _WorkersManager.Workers;
+
+        public MainWindowViewModel(WorkersManager WorkersManager)
         {
-            AddCompany = NewCompany;
-            NewCompany.MainModel = this;
+           _WorkersManager = WorkersManager;
         }
     }
 }
